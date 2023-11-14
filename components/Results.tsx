5897366068ed34f4005dd29fa0da80ab0a766f59
Ludovic Muller <ludovicmuller1@gmail.com>
2023-11-14 11:37:43 +0100

    handle boolean responses

--- components/Results.tsx
+++ components/Results.tsx
@@ -7,6 +7,9 @@ type Props = {
 
 const Results: React.FC<Props> = ({ results }) => {
   if (typeof results !== "string") {
+    if (Object.hasOwnProperty.call(results, "boolean")) {
+      return <p>{results.boolean ? "True" : "False"}</p>;
+    }
     return <pre>{JSON.stringify(results, null, 2)}</pre>;
   } else {
     return <Editor height="50vh" value={results} theme="vs-dark" />;
