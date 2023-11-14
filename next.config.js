f8330ec42caf0f4d7d7fe00133548d198af8c03f
Ludovic Muller <ludovicmuller1@gmail.com>
2023-11-14 12:07:56 +0100

    export generated files in out directory

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withTM = require('next-transpile-modules')(['oxigraph']);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
 };
 
 module.exports = withTM(nextConfig);
