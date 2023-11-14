3b57b00bd1482c165a2921a385799947928b7145
ludovicm67 <ludovicm67@users.noreply.github.com>
2023-11-14 11:21:19 +0000

    Deploying to gh-pages from @ ludovicm67/sparql-playground@16484eb63e2c8e8b59d28a5ef23cc35bfc11d4f7 🚀

---
+++ _next/static/J7Yy2MCHPSYO4bZ39GJ0Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/529-d2906a652d120792.js","static/chunks/pages/index-5c212d658406904a.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
