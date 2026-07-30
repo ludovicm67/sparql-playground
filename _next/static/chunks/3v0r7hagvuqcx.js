e26fead04220d9f83c737db56d13e8ab735b5b7b
ludovicm67 <ludovicm67@users.noreply.github.com>
2026-07-30 14:48:58 +0000

    Deploying to gh-pages from @ ludovicm67/sparql-playground@6d37e8a928c5a7183c0f7c2e98a2a05fefffd88c 🚀

--- _next/static/chunks/3v0r7hagvuqcx.js
+++ _next/static/chunks/3v0r7hagvuqcx.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/", [
   "static/chunks/3c14yg4d5k6cs.js",
   "static/chunks/33liw_ygai4as.js",
-  "static/chunks/0_3df890v7iih.js",
-  "static/chunks/turbopack-0ijfeso55bpmg.js"
+  "static/chunks/19h3smay6rm68.js",
+  "static/chunks/turbopack-3896581yanv0j.js"
 ])
